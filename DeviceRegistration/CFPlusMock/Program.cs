825e9b090fcb8b93917e3492c9da8c0d5422163d
Barret Ang Xiong <AXiong3@rockwellautomation.com>
2020-12-02 11:15:21 +0800

    First commit

---
+++ DeviceRegistration/CFPlusMock/Program.cs
@@ -0,0 +1,34 @@
+﻿using DeviceRegistion;
+using Grpc.Net.Client;
+using System;
+
+namespace CFPlusMock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Create DataService grpc client
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new DataService.DataServiceClient(channel);
+
+
+            //Call grpc functions
+            StatusRespondData statusRespond = client.Status(new Empty());
+            if(statusRespond.IsSucceed)
+                Console.WriteLine("Status: " + statusRespond.Status);
+
+            AuthRespondData authRespond = client.Auth(new AuthRequestData());
+            if(authRespond.IsSucceed)
+                Console.WriteLine("Auth Token: " + authRespond.AuthToken);
+
+            SaveRespondData saveRespond = client.Save(new SaveRequestData());
+            if(saveRespond.IsSucceed)
+                Console.WriteLine("Save Message: " + saveRespond.Message);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DeviceRegistration/GrpcServiceMock/Services/DeviceRegistionService.cs
@@ -0,0 +1,44 @@
+using DeviceRegistion;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcServiceMock
+{
+    public class DeviceRegistionService : DataService.DataServiceBase
+    {
+        private readonly ILogger<DeviceRegistionService> _logger;
+        public DeviceRegistionService(ILogger<DeviceRegistionService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<StatusRespondData> Status(Empty request, ServerCallContext context)
+        {
+            return Task.FromResult(new StatusRespondData
+            {
+                IsSucceed = true,
+                Status = SeverStatusEnum.Available
+            });
+        }
+
+        public override Task<AuthRespondData> Auth(AuthRequestData request, ServerCallContext context)
+        {
+            return Task.FromResult(new AuthRespondData
+            {
+                IsSucceed = true,
+                AuthToken = Guid.NewGuid().ToString()
+            });
+        }
+
+        public override Task<SaveRespondData> Save(SaveRequestData request, ServerCallContext context)
+        {
+            return Task.FromResult(new SaveRespondData
+            {
+                IsSucceed = true,
+                Message = "Succeed"
+            });
+        }
+    }
+}
